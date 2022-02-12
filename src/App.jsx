656d1f16c42538e30d8718a258289b06942d27b5
Maximiliano Alvarenga <maxbispo163@hotmail.com>
2022-02-11 21:27:17 -0400

    finalizado botão de login e redapé com links.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { AiOutlineArrowRight } from "react-icons/ai";
 import './App.css';
 import video from './assets/login.mp4';
 import videoLoad from './assets/riotLoad.mp4';
@@ -9,7 +10,7 @@ import appleLogin from './assets/apple-icon.png';
 
 function App() {
   const [load, setLoad] = useState(false);
-  const delay = 11;
+  const delay = 1;
 
   useEffect(() => {
     let timer = setTimeout(() => setLoad(true), delay * 1000);
@@ -24,28 +25,42 @@ function App() {
     <section className="main">
       <aside className="left-content">
         <div className="form-auth">
-          <img src={leagueIcon} alt="League Of Legends icon" />
+          <img src={leagueIcon} alt="League Of Legends icon" className='icon-riot'/>
           <span>Fazer login</span>
           <section className="login">
-            <input type="text" placeholder="NOME DE USUÁRIO" />
-            <input type="password" placeholder="SENHA" />
-            <section className='social'>
-              <div id='facebook'>
-                <img src={facebookLogin} alt="Facebook icon" />
-              </div>
-              <div id='google'>
-                <img src={googleLogin} alt="Google icon" />
-              </div>
-              <div id='apple'>
-                <img src={appleLogin} alt="Apple icon" />
-              </div>
-            </section>
-            <label class="container">Manter login
-              <input type="checkbox" />
-              <span class="mark"></span>
-            </label>
+            <div>
+              <input type="text" placeholder="NOME DE USUÁRIO" />
+              <input type="password" placeholder="SENHA" />
+              <section className='social'>
+                <div id='facebook'>
+                  <img src={facebookLogin} alt="Facebook icon" className='icon-social'/>
+                </div>
+                <div id='google'>
+                  <img src={googleLogin} alt="Google icon" />
+                </div>
+                <div id='apple'>
+                  <img src={appleLogin} alt="Apple icon" />
+                </div>
+              </section>
+              <label class="container">Manter login
+                <input type="checkbox" />
+                <span class="mark"></span>
+              </label>
+            </div>
+            <div className='box-btn-login'>
+              <AiOutlineArrowRight className='btn-login' />
+            </div>
           </section>
         </div>
+        <section id='footer-right'>
+            <div className='user-help'>
+              <span>NÃO CONSEGUE INICIAR SESSÃO?</span>
+              <span>CRIAR CONTA</span>
+            </div>
+            <div className='version-app'>
+              <span>V0.0.1</span>
+            </div>
+          </section>
       </aside>
       <section className="">
         <video autoPlay={true} loop={true} className="video-background" muted={true}>
